d32eeb2b9cbc84911c5f1bebaaa12c8d1b95091f
jorge.hermes <jorge.hermes@movtech.com.br>
2026-01-05 16:48:57 -0300

    Primeiro commit

---
+++ WindowsFormsApp1/FrmCliente.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CRUDbiblioteca.enums;
+
+namespace CRUDbiblioteca
+{
+    public partial class FrmCliente : Form
+    {
+        private OpcaoCliente acaoAtual = OpcaoCliente.Nenhuma;
+        public FrmCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void excluir_Click(object sender, EventArgs e)
+        {
+            acaoAtual = OpcaoCliente.Excluir;
+        }
+
+        private void cadastrar_Click(object sender, EventArgs e)
+        {
+            acaoAtual = OpcaoCliente.Cadastrar;
+        }
+
+        private void atualizar_Click(object sender, EventArgs e)
+        {
+            acaoAtual = OpcaoCliente.Atualizar;
+        }
+
+
+    }
+}
---
+++ WindowsFormsApp1/enums/OpcaoCliente.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDbiblioteca.enums
+{
+    public enum OpcaoCliente
+    {Nenhuma,Cadastrar,Atualizar,Excluir}
+}
